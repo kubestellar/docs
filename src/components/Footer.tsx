424d5f0a6ffd4b1c4f37e48ce8100fc0d4b5e8a9
Vedansh Saini <77830698+vedansh-5@users.noreply.github.com>
2025-09-13 13:29:30 +0530

    matched logos
    Signed-off-by: Vedansh Saini <77830698+vedansh-5@users.noreply.github.com>

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -157,10 +157,11 @@ export default function Footer() {
 
               <a
                 href="#"
-                className="w-10 h-10 bg-gray-700 hover:bg-purple-600 rounded-lg flex items-center justify-center transition-colors duration-300"
+                className="w-7 h-7 rounded-lg flex items-center justify-center transition-colors duration-300"
               >
                 <svg
-                  className="w-7 h-7"
+                  opacity={0.5}
+                  className="w-6 h-6 hover:opacity-75"
                   fill="currentColor"
                   viewBox="0 0 24 24"
                 >
@@ -169,36 +170,39 @@ export default function Footer() {
               </a>
               <a
                 href="#"
-                className="w-10 h-10 bg-gray-700 hover:bg-blue-600 rounded-lg flex items-center justify-center transition-colors duration-300"
+                className="w-7 h-7 rounded-lg flex items-center justify-center transition-colors duration-300"
               >
                 <svg
-                  className="w-7 h-7"
+                  className="w-6 h-6 hover:opacity-75"
                   fill="currentColor"
                   viewBox="0 0 24 24"
+                  opacity={0.5}
                 >
                   <path d="M12 0C5.374 0 0 5.373 0 12 0 17.302 3.438 21.8 8.207 23.387c.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23A11.509 11.509 0 0112 5.803c1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576C20.566 21.797 24 17.300 24 12c0-6.627-5.373-12-12-12z" />
                 </svg>
               </a>
               <a
                 href="#"
-                className="w-10 h-10 bg-gray-700 hover:bg-blue-500 rounded-lg flex items-center justify-center transition-colors duration-300"
+                className="w-7 h-7 rounded-lg flex items-center justify-center transition-colors duration-300"
               >
                 <svg
-                  className="w-7 h-7"
+                  className="w-6 h-6 hover:opacity-75"
                   fill="currentColor"
                   viewBox="0 0 24 24"
+                  opacity={0.5}
                 >
                   <path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z" />
                 </svg>
               </a>
               <a
                 href="#"
-                className="w-10 h-10 bg-gray-700 hover:bg-red-600 rounded-lg flex items-center justify-center transition-colors duration-300"
+                className="w-7 h-7 rounded-lg flex items-center justify-center transition-colors duration-300"
               >
                 <svg
-                  className="w-7 h-7"
+                  className="w-6 h-6 hover:opacity-75"
                   fill="currentColor"
                   viewBox="0 0 24 24"
+                  opacity={0.5}
                 >
                   <path d="M23.498 6.186a3.016 3.016 0 0 0-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 0 0 .502 6.186C0 8.07 0 12 0 12s0 3.93.502 5.814a3.016 3.016 0 0 0 2.122 2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 0 0 2.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z" />
                 </svg>
@@ -328,12 +332,12 @@ export default function Footer() {
             <h3 className="text-lg font-semibold text-white mb-4">Stay Updated</h3>
             <div className="border border-gray-700 bg-[#161A23] rounded-xl p-6">
               <form action="" className="space-y-6">
-                <div className="relative ">
+                <div className="relative">
                   <svg className="absolute left-3 top-1/2 -translate-y-1/2 h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-                    <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
-                    <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
+                  <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
+                  <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
                   </svg>
-                  <input type="email" placeholder="Enter your email address" className=" w-full bg-[#252C39] border border-gray-700 rounded-lg py-5 text-wrap pl-10 pr-4 text-white placeholder-gray-400 focus:ring-purple-500 focus:border-purple-500 transition" />
+                  <input type="email" placeholder="Enter your email" className="w-full bg-[#252C39] border border-gray-700 rounded-lg py-2.5 pl-10 pr-4 text-white placeholder-gray-400 focus:ring-purple-500 focus:border-purple-500 transition" />
                 </div>
                 <button type="submit" className="w-full bg-gradient-to-r from-blue-600 to-purple-600 hover:from-blue-700 hover:to-purple-700 text-white font-bold py-2.5 px-4 rounded-lg flex items-center justify-center transition-all duration-300">Subscribe
                   <svg className="w-5 h-5 ml-2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
