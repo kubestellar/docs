7d305d6eba3efde0f5f8988b02dc3e1bca63237e
naman9271 <namanjain9271@gmail.com>
2025-11-06 22:39:18 +0530

    fixing loading

--- src/app/[locale]/playground/page.tsx
+++ src/app/[locale]/playground/page.tsx
@@ -2,6 +2,7 @@
 
 import { useEffect, useState } from "react";
 import { useRouter } from "@/i18n/navigation";
+import Loader from "@/components/animations/loader";
 
 export default function PlaygroundPage() {
   const router = useRouter();
@@ -14,13 +15,6 @@ export default function PlaygroundPage() {
   }, [router]);
 
   return(
-    <>
-    <div className="flex items-center justify-center h-screen bg-black">
-      <div className="flex flex-col items-center space-y-4">
-        <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-blue-600"></div>
-        <p className="text-lg text-gray-300">Loading...</p>
-      </div>
-    </div>
-    </>
+    <Loader/>
   );
 }
