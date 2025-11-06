d254134a55e4d10d8f541327fa82b767f7ad0f29
naman9271 <namanjain9271@gmail.com>
2025-11-06 05:56:29 +0530

    fixing logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -626,7 +626,7 @@ export default function Navbar() {
                           strokeLinecap="round"
                           strokeLinejoin="round"
                           strokeWidth="2"
-                          d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2H5a2 2 0 00-2 2v2M7 7h10"
+                          d="M19 21V5a2 2 0 00-2-2H7a2 2 0 00-2 2v16m14 0h2m-2 0h-5m-9 0H3m2 0h5M9 7h1m-1 4h1m4-4h1m-1 4h1m-5 10v-5a1 1 0 011-1h2a1 1 0 011 1v5m-4 0h4"
                         ></path>
                       </svg>
                       {t("programs")}
