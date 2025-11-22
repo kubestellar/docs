8a95a80031c2cee462f26c792d432acbaf2e1b1c
naman9271 <namanjain9271@gmail.com>
2025-11-22 22:25:02 +0530

    miscellaneous chanegs

--- src/app/[locale]/playground/page.tsx
+++ src/app/[locale]/playground/page.tsx
@@ -6,11 +6,7 @@ import Loader from "@/components/animations/Loader";
 
 export default function PlaygroundPage() {
   const router = useRouter();
-  //eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const [isRedirecting, setIsRedirecting] = useState(true);
-
   useEffect(() => {
-    // Redirect immediately to coming-soon page
     router.replace("/coming-soon");
   }, [router]);
 
