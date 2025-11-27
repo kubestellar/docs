c1b196f76ddb4d71dfa25110a778dbbea2a2ab43
arpit529srivastava <arpitsrivastava529@gmail.com>
2025-11-27 20:29:12 +0530

    fix: update kubectl-multi repository URL
    Signed-off-by: arpit529srivastava <arpitsrivastava529@gmail.com>

--- src/app/[locale]/products/page.tsx
+++ src/app/[locale]/products/page.tsx
@@ -60,7 +60,7 @@ export default function ProductsPage() {
       id: "kubectl-multi",
       logo: "/products/kubectl-multi.png",
       website: "https://github.com/kubestellar/kubectl-multi",
-      repository: "https://github.com/kubestellar/kubectl-multi",
+      repository: "https://github.com/kubestellar/kubectl-multi-plugin",
       name: t("products.kubectlMulti.name"),
       fullName: t("products.kubectlMulti.fullName"),
       description: t("products.kubectlMulti.description"),
