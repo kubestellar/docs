a6526f9903278359ce538e34e568f08844c8b7bd
Kevin Roche <kproche@us.ibm.com>
2025-12-04 09:34:08 -0800

    trying different syntax in proxy.ts
    Signed-off-by: Kevin Roche <kproche@us.ibm.com>

--- proxy.ts
+++ proxy.ts
@@ -1,10 +1,26 @@
-export { proxy } from 'nextra/locales'
+import createMiddleware from 'next-intl/middleware';
+import {NextRequest} from 'next/server';
+ 
+export default async function proxy(request: NextRequest) {
+  // Step 1: Use the incoming request (example)
+  const defaultLocale = request.headers.get('x-your-custom-locale') || 'en';
+ 
+  // Step 2: Create and call the next-intl middleware (example)
+  const handleI18nRouting = createMiddleware({
+    locales: ['en', 'de'],
+    defaultLocale
+  });
+  const response = handleI18nRouting(request);
+ 
+  // Step 3: Alter the response (example)
+  response.headers.set('x-your-custom-locale', defaultLocale);
+ 
+  return response;
+}
  
 export const config = {
-  // Matcher ignoring `/_next/` and `/api/`
-  matcher: [
-    '/((?!docs|api|_next|_vercel|agenda|blog|code|community|drive|infomercial|join_us|joinus|ladder|ladder_stats|linkedin|quickstart|slack|survey|tv|youtube|.*\\..*).*)', 
-
-//    '/((?!api|_next/static|_next/image|favicon.ico|icon.svg|apple-icon.png|manifest|_pagefind).*)'
+  // Match only internationalized pathnames
+  matcher: ['
+    /((?!docs|api|_next|_vercel|agenda|blog|code|community|drive|infomercial|join_us|joinus|ladder|ladder_stats|linkedin|quickstart|slack|survey|tv|youtube|.*\\..*).*)',
   ]
 }
