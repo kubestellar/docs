ab4e6766294544b014bf0abdbaffa7c9528ce03b
Kevin Roche <kproche@us.ibm.com>
2026-02-26 13:54:18 -0800

    fixing Docs home link color
    Signed-off-by: Kevin Roche <kproche@us.ibm.com>

--- src/components/docs/DocsSidebar.tsx
+++ src/components/docs/DocsSidebar.tsx
@@ -250,7 +250,7 @@ export function DocsSidebar({ pageMap, className }: DocsSidebarProps) {
         <div className="px-4 pt-4 pb-2 border-b border-gray-200 dark:border-gray-800">
           <Link
             href="/docs"
-            className="text-sm font-medium text-gray-700 hover:text-gray-900 dark:text-gray-200 dark:hover:text-white"
+            className="text-sm font-medium text-gray-400 hover:text-gray-900 dark:text-white dark:hover:text-white"
           >
             Docs Home
           </Link>
